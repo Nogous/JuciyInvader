2f399c0d9ee9eda85758713fdc3eaee96fc605c5
Camille <camille.lavarde@edu.devinci.fr>
2021-01-04 13:03:23 +0100

    player and shoot

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public Vector3 direction;
+    public float speed = .1f;
+
+    public float liveDuration = 5f;
+
+    public bool activate;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (activate)
+        {
+            transform.position += direction * speed;
+        }
+
+        liveDuration -= Time.deltaTime;
+        if (liveDuration<=0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
