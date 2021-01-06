0749682203f97add456254e41daae1568cb2fce8
6meon <malaretsimeon@gmail.com>
2021-01-06 10:20:47 +0100

    Win/Lose Condition

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,12 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager instance;
 
     public bool azertyControle;
+
+    public bool isPlayerDead = false;
+    public bool AreEnemiesLeft = true;
+
+    public bool controlsEnabled = true;
+
+    public GameObject winText;
+    public GameObject losetext;
+    public GameObject restartText;
 
     [Header("Juicy Parameter")]
     public bool enemyFade;
@@ -28,6 +38,26 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(!AreEnemiesLeft)
+        {
+            winText.SetActive(true);
+            restartText.SetActive(true);
+            controlsEnabled = false;
+            if(Input.GetKeyDown(KeyCode.R))
+            {
+                SceneManager.LoadScene("SimeonScene 1");
+            }
+        }
+
+        if(isPlayerDead)
+        {
+            losetext.SetActive(true);
+            restartText.SetActive(true);
+            controlsEnabled = false;
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+                SceneManager.LoadScene("SimeonScene 1");
+            }
+        }
     }
 }
